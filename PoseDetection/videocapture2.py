3f5cb9ace5b695e96b42efaa99795e3b945e1eef
Ric Ian Jamora <yan2_1447@yahoo.com>
2023-01-22 23:11:39 +0800

    new

---
+++ PoseDetection/videocapture2.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+capture = cv2.VideoCapture(0)
+codec = cv2.VideoWriter_fourcc(*'XVID')
+
+recording_flag = False
+
+while True:
+    ret, frame_temp = capture.read()
+    cv2.imshow('FRAME', frame_temp)
+    key=cv2.waitKey(1)
+    if key%256 == 27:
+        break
+    elif cv2.waitKey(1) & 0xFF == ord('r'):
+        if recording_flag == False:
+            print('start')
+            # we are transitioning from not recording to recording
+            output = cv2.VideoWriter('CAPTURE1.mp4', -1, 30, (640, 480))
+            recording_flag = True
+        else:
+            # transitioning from recording to not recording
+            print("done")
+            recording_flag = False
+
+    if recording_flag:
+        output.write(frame_temp)
+
+capture.release()
+output.release()
+cv2.destroyAllWindows()
